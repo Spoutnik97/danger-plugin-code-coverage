b6c2b8f6d70d6cf081070d6e30e1731952174557
Joshua Kaplan <yhkaplan@gmail.com>
2022-07-26 19:47:31 -0700

    Replace links with emoji

--- src/lib/formatCoverageState.ts
+++ src/lib/formatCoverageState.ts
@@ -2,19 +2,11 @@ import { roundPercentage } from "./roundPercentage"
 
 export const formatCoverageState = (stat: number) => {
   const percentage = roundPercentage(stat * 100)
-
-  const GREEN = "00ff00"
-  const RED = "ff0000"
-  const ORANGE = "ffaa00"
-
-  let color = GREEN
-
   if (percentage < 80) {
-    color = ORANGE
+    return "🟧"
+  } else if (percentage < 50) {
+    return "🟥"
+  } else {
+    return "🟩"
   }
-  if (percentage < 50) {
-    color = RED
-  }
-
-  return `![${percentage}](https://via.placeholder.com/15/${color}/000000?text=+) ${percentage}%`
 }
